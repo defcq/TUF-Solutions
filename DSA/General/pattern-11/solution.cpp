e37bc947121686bbfc02cecb9a0837c58fecc3fc
defcq <deeps122006@gmail.com>
2026-08-27 01:10:46 +0530

    Sync Pattern 11 [DSA] - TUFHub

---
+++ DSA/General/pattern-11/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void pattern11(int n) {
+        int start ;
+        for (int i=1;i<=n;i++){
+            if (i%2==0) start = 0;
+            else start = 1;
+            for(int j=1;j<=i;j++){
+                cout << start;
+                cout << " ";
+                start = 1 - start;
+            }
+            cout << endl;
+        }
+
+    }
+};
