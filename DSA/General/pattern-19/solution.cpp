6de64f350ad0b98a7d5b71983ee1d210120cab6c
defcq <deeps122006@gmail.com>
2026-08-31 21:29:08 +0530

    Sync Pattern 19 [DSA] - TUFHub

---
+++ DSA/General/pattern-19/solution.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void pattern19(int n) {
+ for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - i; j++) {
+            cout << "*";
+        }
+
+        for (int j = 0; j < 2 * i; j++) {
+            cout << " ";
+        }
+
+        for (int j = 0; j < n - i; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    for (int i = n - 1; i >= 0; i--) {
+        for (int j = 0; j < n - i; j++) {
+            cout << "*";
+        }
+
+        for (int j = 0; j < 2 * i; j++) {
+            cout << " ";
+        }
+
+        for (int j = 0; j < n - i; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+    }
+};
